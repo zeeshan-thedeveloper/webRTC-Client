aff0dd334c5741bda1647240fef63c4d200d24ba
Zeeshan <zeeshanahmedd0010@gmail.com>
2023-04-04 00:45:42 +0500

    created dashboard showing up video and controls

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,27 @@
-const Dashboard=()=>{
-    return <div>Dashboard</div>
-}
+import { useState } from "react";
+import "../styles/dashboard.css";
 
-export default Dashboard
+const Dashboard = () => {
+  const [isMicOn, setIsMicOn] = useState(true);
+  const [isVideoOn, setIsVideoOn] = useState(true);
+    
+  return (
+    <div className="Dashboard">
+      <h2>Call with John</h2>
+      <div className="video-container">
+        <video className="video" autoPlay></video>
+        <video className="local-video" autoPlay muted></video>
+      </div>
+      <div className="controls">
+        <button onClick={() => setIsMicOn(!isMicOn)}>
+          {isMicOn ? "🎤" : "🎤🔕"}
+        </button>
+        <button onClick={() => setIsVideoOn(!isVideoOn)}>
+          {isVideoOn ? "🎥" : "🎥🔕"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
